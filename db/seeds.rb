72d23a9efcec46723c38e8ef497542e0f4983f84
Olivier Volpi <olivolpi@gmail.com>
2024-02-21 12:48:24 +0100

    restau en cours

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+pizza_tony = {name: "Pizza Tony", address: "3 rue de l'Italie", phone_number: "0542372878", category: "italien"}
+thai_miam = {name: "Thai Miam", address: "5 avenue de Bangkok", phone_number: "1233457", category: "thai"}
+sushi_top = {name: "Sushi Top", address: "4 place de Tokyo", phone_number: "234748854", category: "japanese"}
+
+[pizza_tony, thai_miam, sushi_top].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
